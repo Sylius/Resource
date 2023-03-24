b36368069c5ea39ea60ad593523fc49bdfbd7d45
Loïc Frémont <lc.fremont@gmail.com>
2023-03-24 08:38:34 +0100

    Move operation route factory

---
+++ Symfony/Routing/Factory/OperationRouteFactory.php
@@ -0,0 +1,132 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Resource\Symfony\Routing\Factory;
+
+use Gedmo\Sluggable\Util\Urlizer;
+use Sylius\Component\Resource\Action\PlaceHolderAction;
+use Sylius\Component\Resource\Metadata\BulkOperationInterface;
+use Sylius\Component\Resource\Metadata\CollectionOperationInterface;
+use Sylius\Component\Resource\Metadata\CreateOperationInterface;
+use Sylius\Component\Resource\Metadata\DeleteOperationInterface;
+use Sylius\Component\Resource\Metadata\HttpOperation;
+use Sylius\Component\Resource\Metadata\MetadataInterface;
+use Sylius\Component\Resource\Metadata\Resource;
+use Sylius\Component\Resource\Metadata\ShowOperationInterface;
+use Sylius\Component\Resource\Metadata\UpdateOperationInterface;
+use Symfony\Component\Routing\Route;
+
+final class OperationRouteFactory implements OperationRouteFactoryInterface
+{
+    public function create(MetadataInterface $metadata, Resource $resource, HttpOperation $operation): Route
+    {
+        $routePath = $operation->getPath() ?? $this->getDefaultRoutePath($metadata, $operation);
+
+        if (null !== $routePrefix = $operation->getRoutePrefix()) {
+            $routePath = $routePrefix . '/' . $routePath;
+        }
+
+        return new Route(
+            path: $routePath,
+            defaults: [
+                '_controller' => PlaceHolderAction::class,
+                '_sylius' => $this->getSyliusOptions($resource, $operation),
+            ],
+            methods: $operation->getMethods() ?? [],
+        );
+    }
+
+    private function getDefaultRoutePath(MetadataInterface $metadata, HttpOperation $operation): string
+    {
+        return $this->getDefaultRoutePathForOperation($metadata, $operation);
+    }
+
+    private function getDefaultRoutePathForOperation(MetadataInterface $metadata, HttpOperation $operation): string
+    {
+        $rootPath = sprintf('%s', Urlizer::urlize($metadata->getPluralName()));
+
+        if (null !== $path = $operation->getPath()) {
+            return $path;
+        }
+
+        if (null === $shortName = $operation->getShortName()) {
+            throw new \InvalidArgumentException(sprintf('Operation "%s" should have a short name. Please define one.', $operation::class));
+        }
+
+        $identifier = $operation->getResource()?->getIdentifier() ?? 'id';
+
+        if ($operation instanceof CollectionOperationInterface) {
+            $path = match ($shortName) {
+                'index', 'get_collection' => '',
+                default => '/' . $shortName,
+            };
+
+            return sprintf('%s%s', $rootPath, $path);
+        }
+
+        if ($operation instanceof CreateOperationInterface) {
+            $path = match ($shortName) {
+                'create' => '/new',
+                'post' => '',
+                default => '/' . $shortName,
+            };
+
+            return sprintf('%s%s', $rootPath, $path);
+        }
+
+        if ($operation instanceof UpdateOperationInterface) {
+            $path = match ($shortName) {
+                'update' => '/edit',
+                'put', 'patch' => '',
+                default => '/' . $shortName,
+            };
+
+            return sprintf('%s/{%s}%s', $rootPath, $identifier, $path);
+        }
+
+        if ($operation instanceof BulkOperationInterface) {
+            return sprintf('%s/%s', $rootPath, $shortName);
+        }
+
+        if ($operation instanceof DeleteOperationInterface) {
+            $path = match ($shortName) {
+                'delete' => '',
+                default => '/' . $shortName,
+            };
+
+            return sprintf('%s/{%s}%s', $rootPath, $identifier, $path);
+        }
+
+        if ($operation instanceof ShowOperationInterface) {
+            $path = match ($shortName) {
+                'show', 'get' => '',
+                default => '/' . $shortName,
+            };
+
+            return sprintf('%s/{%s}%s', $rootPath, $identifier, $path);
+        }
+
+        throw new \InvalidArgumentException(sprintf('Impossible to get a default route path for this operation "%s". Please define a path.', $operation::class));
+    }
+
+    private function getSyliusOptions(Resource $resource, HttpOperation $operation): array
+    {
+        $options = ['resource' => $resource->getAlias()];
+
+        if (null !== $section = $resource->getSection()) {
+            $options['section'] = $section;
+        }
+
+        return $options;
+    }
+}
---
+++ spec/Symfony/Routing/Factory/OperationRouteFactorySpec.php
@@ -0,0 +1,520 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace spec\Sylius\Component\Resource\Symfony\Routing\Factory;
+
+use PhpSpec\ObjectBehavior;
+use Sylius\Component\Resource\Action\PlaceHolderAction;
+use Sylius\Component\Resource\Metadata\Api;
+use Sylius\Component\Resource\Metadata\BulkDelete;
+use Sylius\Component\Resource\Metadata\Create;
+use Sylius\Component\Resource\Metadata\Delete;
+use Sylius\Component\Resource\Metadata\HttpOperation;
+use Sylius\Component\Resource\Metadata\Index;
+use Sylius\Component\Resource\Metadata\Metadata;
+use Sylius\Component\Resource\Metadata\Resource;
+use Sylius\Component\Resource\Metadata\Show;
+use Sylius\Component\Resource\Metadata\Update;
+use Sylius\Component\Resource\Symfony\Routing\Factory\OperationRouteFactory;
+
+final class OperationRouteFactorySpec extends ObjectBehavior
+{
+    function it_is_initializable(): void
+    {
+        $this->shouldHaveType(OperationRouteFactory::class);
+    }
+
+    function it_generates_create_routes(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Create(),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/new');
+        $route->getMethods()->shouldReturn(['GET', 'POST']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_create_routes_with_custom_short_name(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Create(shortName: 'register'),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/register');
+        $route->getMethods()->shouldReturn(['GET', 'POST']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_api_post_routes(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Api\Post(),
+        );
+
+        $route->getPath()->shouldReturn('/dummies');
+        $route->getMethods()->shouldReturn(['POST']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_index_routes(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Index(),
+        );
+
+        $route->getPath()->shouldReturn('/dummies');
+        $route->getMethods()->shouldReturn(['GET']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_index_routes_with_custom_short_name(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Index(shortName: 'list'),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/list');
+        $route->getMethods()->shouldReturn(['GET']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_api_get_collection_routes(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Api\GetCollection(),
+        );
+
+        $route->getPath()->shouldReturn('/dummies');
+        $route->getMethods()->shouldReturn(['GET']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_show_routes(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Show(),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{id}');
+        $route->getMethods()->shouldReturn(['GET']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_show_routes_with_custom_short_name(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Show(shortName: 'details'),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{id}/details');
+        $route->getMethods()->shouldReturn(['GET']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_api_get_routes(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Api\Get(),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{id}');
+        $route->getMethods()->shouldReturn(['GET']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_show_routes_with_custom_identifier(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $resource = new Resource('app.dummy', identifier: 'code');
+
+        /** @var HttpOperation $operation */
+        $operation = (new Show())->withResource($resource);
+
+        $route = $this->create(
+            $metadata,
+            $resource,
+            $operation,
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{code}');
+        $route->getMethods()->shouldReturn(['GET']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_update_routes(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Update(),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{id}/edit');
+        $route->getMethods()->shouldReturn(['GET', 'PUT']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_update_routes_with_custom_identifier(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $resource = new Resource('app.dummy', identifier: 'code');
+
+        /** @var HttpOperation $operation */
+        $operation = (new Update())->withResource($resource);
+
+        $route = $this->create(
+            $metadata,
+            $resource,
+            $operation,
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{code}/edit');
+        $route->getMethods()->shouldReturn(['GET', 'PUT']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_update_routes_with_custom_short_name(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Update(shortName: 'edition'),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{id}/edition');
+        $route->getMethods()->shouldReturn(['GET', 'PUT']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_api_put_routes(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Api\Put(),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{id}');
+        $route->getMethods()->shouldReturn(['PUT']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_api_patch_routes(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Api\Patch(),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{id}');
+        $route->getMethods()->shouldReturn(['PATCH']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_delete_routes(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Delete(),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{id}');
+        $route->getMethods()->shouldReturn(['DELETE']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_delete_routes_with_custom_short_name(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Delete(shortName: 'remove'),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{id}/remove');
+        $route->getMethods()->shouldReturn(['DELETE']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_api_delete_routes(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new Api\Delete(),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{id}');
+        $route->getMethods()->shouldReturn(['DELETE']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_delete_routes_with_custom_identifier(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $resource = new Resource('app.dummy', identifier: 'code');
+
+        /** @var HttpOperation $operation */
+        $operation = (new Delete())->withResource($resource);
+
+        $route = $this->create(
+            $metadata,
+            $resource,
+            $operation,
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{code}');
+        $route->getMethods()->shouldReturn(['DELETE']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_bulk_delete_routes(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new BulkDelete(),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/bulk_delete');
+        $route->getMethods()->shouldReturn(['DELETE']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_custom_operations_routes(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource('app.dummy'),
+            new HttpOperation(methods: ['PATCH'], path: 'dummies/{id}/custom'),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{id}/custom');
+        $route->getMethods()->shouldReturn(['PATCH']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+            ],
+        ]);
+    }
+
+    function it_generates_routes_with_sections(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $route = $this->create(
+            $metadata,
+            new Resource(alias: 'app.dummy', section: 'admin'),
+            new Show(),
+        );
+
+        $route->getPath()->shouldReturn('/dummies/{id}');
+        $route->getMethods()->shouldReturn(['GET']);
+        $route->getDefaults()->shouldReturn([
+            '_controller' => PlaceHolderAction::class,
+            '_sylius' => [
+                'resource' => 'app.dummy',
+                'section' => 'admin',
+            ],
+        ]);
+    }
+
+    function it_throws_an_exception_when_operation_does_not_have_short_name(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $this->shouldThrow(new \InvalidArgumentException(sprintf('Operation "%s" should have a short name. Please define one.', HttpOperation::class)))->during(
+            'create',
+            [
+                $metadata,
+                new Resource('app.dummy'),
+                new HttpOperation(),
+            ],
+        );
+    }
+
+    function it_throws_an_exception_when_operation_does_not_have_path(): void
+    {
+        $metadata = Metadata::fromAliasAndConfiguration('app.dummy', ['driver' => 'dummy_driver']);
+
+        $this->shouldThrow(new \InvalidArgumentException(sprintf('Impossible to get a default route path for this operation "%s". Please define a path.', HttpOperation::class)))->during(
+            'create',
+            [
+                $metadata,
+                new Resource('app.dummy'),
+                new HttpOperation(shortName: 'custom'),
+            ],
+        );
+    }
+}
